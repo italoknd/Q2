baaf4270f4931f00a4ad3dfe5a62879f3dca5d60
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-05-02 14:15:44 -0400

    validação de idade realizada

---
+++ validacao_de_idade/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <Mensagem :mensagem="aviso" :background="background" v-show="checarIdade" />
+    <div class="mb-3 form">
+      <label class="form-label">Informe sua idade: </label>
+      <input type="number" class="form-control" v-model="idade"/>
+      <button
+        type="submit"
+        class="btn btn-primary mb-3 mt-2"
+        @click="checkIdade()"
+      >
+        Checar Idade
+      </button>
+    </div>
+    <div class="form">
+      <p v-show="aviso">Idade informada: {{idade}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Mensagem from './components/Mensagem.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      background: '',
+      checarIdade: false,
+      aviso: '',
+      idade: '',
+    }
+  },
+  components: {
+    Mensagem
+  },
+  methods: {
+    checkIdade() {
+      if (this.idade >= 18) {
+        this.aviso = 'Maior de idade'
+        this.checarIdade = true
+        this.background = '#44AF69'
+
+        setTimeout(() => {
+          this.aviso = ''
+          this.checarIdade = false
+        }, 4000)
+      } else {
+        this.aviso = 'Menor de idade'
+        this.checarIdade = true
+        this.background = 'red'
+
+        setTimeout(() => {
+          this.aviso = ''
+          this.checarIdade = false
+        }, 4000)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  flex-direction: column;
+  max-width: 400px;
+}
+
+label {
+  text-align: left !important;
+  font-weight: 600;
+}
+
+p{
+  font-weight: 600;
+}
+</style>
---
+++ validacao_de_idade/src/components/Mensagem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :style="{background: background}" class="mensagem">
+    <p>{{ mensagem }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mensagem',
+  props: {
+    mensagem: String,
+    background: String
+  }
+}
+</script>
+
+<style scoped>
+.mensagem {
+  max-width: 400px;
+  padding: 0.8em;
+  color: white;
+  border-radius: 4px;
+  box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.315);
+  margin: 20px auto;
+  animation: fade 4s;
+}
+
+p {
+  text-align: center;
+  font-weight: 600;
+  margin: auto;
+}
+
+@keyframes fade {
+  25% {
+    opacity: 1;
+    margin: 20px auto;
+  }
+  50% {
+    opacity: 1;
+    margin: 20px auto;
+  }
+  75% {
+    opacity: 0;
+    margin: 20px auto;
+  }
+  100% {
+    opacity: 0;
+    margin: -20px auto;
+  }
+}
+</style>
